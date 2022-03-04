1388ee0f495b50254698c84fffdfcf788cf9ac0f
Walquer Xavier Valles Ruiz <wx.vr@outlook.com>
2022-03-04 13:44:52 -0500

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "deleting subaccount 04" 
-near delete x3.subaccount04.testaccount14.testnet subaccount04.testaccount14.testnet || true
+near delete x4.subaccount04.testaccount14.testnet subaccount04.testaccount14.testnet || true
 echo "re creating subaccount 04"
-near create-account x3.subaccount04.testaccount14.testnet --masterAccount subaccount04.testaccount14.testnet
+near create-account x4.subaccount04.testaccount14.testnet --masterAccount subaccount04.testaccount14.testnet
