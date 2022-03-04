8da220e9ddf8c26b2336f7c2bc5e61b3a87a17aa
WalquerX <wx.vr@outlook.com>
2022-03-04 05:22:44 -0500

    changing account

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@
 mkdir ~/.near-credentials
 cd ~/.near-credentials || exit
 mkdir testnet && cd testnet || exit
-touch fundingaccount05.testnet.json
-echo '{"account_id":"fundingaccount05.testnet","public_key":"ed25519:Fthv63N3mgjbQZLBfzyRQkgghUWhxnMu7dkyy3KFxijs","private_key":"ed25519:1VMwoTWDngRQX992gyFd53ZAALBV4dtmvoiuapmceAYLAFvty63JGrcwEeLshqNgM3cVaXugoQJcfVzHhVvmPgM"}%' > fundingaccount05.testnet.json
+touch subaccount04.testaccount14.testnet.json
+echo '{"account_id":"subaccount04.testaccount14.testnet","public_key":"ed25519:94aDu5tmiLMJQt7g1sAEnZLqpzmHj7k7s4xDrfFUnbmh","private_key":"ed25519:5mQQNvC1B2EZMUNoAqfDVMpk9g6nLZ1RTtAcrwcQBA7ZUYPaLxee4NS7fE5nq1G9V1NwxpX8HV8wvweVbuDiVDAw"}' > subaccount04.testaccount14.testnet.json
