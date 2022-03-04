4774b0b9f8f4c500af1d6db0a77628c022eed79c
Walquer Xavier Valles Ruiz <wx.vr@outlook.com>
2022-03-04 12:52:18 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ mkdir ~/.near-credentials
 cd ~/.near-credentials || exit
 mkdir testnet && cd testnet || exit
 touch subaccount04.testaccount14.testnet.json
-echo '{"account_id":"subaccount04.testaccount14.testnet","public_key":"ed25519:94aDu5tmiLMJQt7g1sAEnZLqpzmHj7k7s4xDrfFUnbmh","private_key":"ed25519:5mQQNvC1B2EZMUNoAqfDVMpk9g6nLZ1RTtAcrwcQBA7ZUYPaLxee4NS7fE5nq1G9V1NwxpX8HV8wvweVbuDiVDAw"}' > subaccount04.testaccount14.testnet.json
+echo '{"account_id":"subaccount04.testaccount14.testnet","public_key":"ed25519:2VwNPCMZK7aMAckkQF8gsNwhY4V1Mg36GcGcRuYgmCG4","private_key":"ed25519:5E2jxWdseXpnWy4a5WjTevo6s8sUstJCTL73hHWxeXkpzv9jTRri59MjfeoA3D2jWFJUKFE1YJjgzC1Aer7vArGc"}' > subaccount04.testaccount14.testnet.json
