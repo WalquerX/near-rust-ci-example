0b36abd57cc9d1478f0287a1a59a171873193d2d
WalquerX <wx.vr@outlook.com>
2022-03-04 04:43:19 -0500

    adding script

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir ~/.near-credentials
+cd ~/.near-credentials || exit
+touch fundingaccount05.testnet.json
+echo "{"account_id":"fundingaccount05.testnet","public_key":"ed25519:Fthv63N3mgjbQZLBfzyRQkgghUWhxnMu7dkyy3KFxijs","private_key":"ed25519:1VMwoTWDngRQX992gyFd53ZAALBV4dtmvoiuapmceAYLAFvty63JGrcwEeLshqNgM3cVaXugoQJcfVzHhVvmPgM"}%" > fundingaccount05.testnet.json
